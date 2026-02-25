40d40b8d58ff8c98bb7fc4486dab0a03d741d744
Grigore Haita <grigore.haita@student.unitbv.ro>
2026-02-25 14:32:21 +0200

    Lab1: HTML and CSS review

---
+++ Lab1/js/contact_form.js
@@ -0,0 +1,9 @@
+function submitForm() {
+    const nume = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const mesaj = document.getElementById("message").value;
+    console.log("Nume: " + nume);
+    console.log("Email: " + email);
+    console.log("Mesaj: " + mesaj);
+    console.warn("Preparing interspace jump...");
+}
