ca9e25ffb1375fa8121ac4c255c51e9ac747ac35
Grigore Haita <grigore.haita@student.unitbv.ro>
2026-03-18 13:19:55 +0200

    feat: Introducere automata a Card in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import './Card';
+import Card from './Card';
 
 function App() {
+  const projects = [
+    { title: "Proiect 1", description: "Pagina personala cu HTML si CSS" },
+    { title: "Proiect 2", description: "Pagina interactiva cu JavaScript" },
+    { title: "Proiect 3", description: "Dashboard cu React" },
+    { title: "Proiect 4", description: "Aplicatie mobila cu React Native" },
+    { title: "Proiect 5", description: "Backend API cu Node.js si Express" },
+  ];
   return (
     <div>
-      <h1>Dashboard</h1>
-      <p>Haita Grigore</p>
-      
-      <Card title = "Proiect 1" description = "Pagina personala cu HTML si CSS"/>
-      <Card title = "Proiect 2" description = "Pagina interactiva cu JavaScript"/>
-      <Card title = "Proiect 3" description = "Dashboard cu React"/>
-      
+      {projects.map((project, index)=>{
+        return <Card key={index} title={project.title} description={project.description} />;
+      })}
+
     </div>
 
 );
