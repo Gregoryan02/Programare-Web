e86784a9c6c18e0024769f8f4767a3fadbe2b8ea
Grigore Haita <grigore.haita@student.unitbv.ro>
2026-03-18 13:10:33 +0200

    feat: Folosirea props pentru componenta

--- src/Educatie.jsx
+++
@@ -1,9 +0,0 @@
-function Educatie() {
-    return (
-    <div>
-        <h2>Educatie</h2>
-        <ul>C.N. Grigore Moisil profilul Stiinte ale Naturii 2020-2024</ul>
-        <ul>Universitatea Transilvania din Brasov faculatea IESC specializarea Calculatoare 2024-prezent</ul>
-    </div>);
-}
-export default Educatie;
