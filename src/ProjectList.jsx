7ccd60e91bbce8e248a3746fea9b3cdd1a73a73b
Grigore Haita <grigore.haita@student.unitbv.ro>
2026-03-25 11:40:17 +0200

    feat: Adauga tratare de erori in projectlist

--- src/ProjectList.jsx
+++ src/ProjectList.jsx
@@ -4,15 +4,24 @@ import Card from './Card';
 function ProjectList() {
     const [projects, setProjects] = useState([]);
     const [loading, setLoading] = useState(true);
+    const[error, setError] = useState(null);
 
     useEffect(() => {
-        fetch('/data/projects.json')
+        fetch('/data/gresit.json')
             .then(response => response.json())
             .then(data => {
                 setProjects(data.projects);
                 setLoading(false);
-            })},[]);
+            })
+            .catch(err => {
+                setError('Eroare la incarcarea proiectelor');
+                setLoading(false);
+                console.warn(err);
+            })
+    }, []);
+
     if(loading) return <p>Loading...</p>;
+    if(error) return <p>{error}</p>;
     return(
         <div>
             <h3>Proiecte</h3>
