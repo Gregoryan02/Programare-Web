d1bd1bc3d40499e29f0c3064ac22431efb97bea1
Haita Grigore <haitagrigore@gmail.com>
2026-04-22 12:46:18 +0300

    feat: adaugat bara de navigare

---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router";
+
+function Navbar(){
+    return(
+        <nav>
+            <NavLink to = "/">Home</NavLink>
+            <NavLink to = "/projects">Projects</NavLink>
+            <NavLink to = "/contact">Contact</NavLink>
+        </nav>
+    );
+}
+
+export default Navbar;
