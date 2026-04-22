406b6757646b05707b1083347e3034a19111ac15
Haita Grigore <haitagrigore@gmail.com>
2026-04-22 12:34:09 +0300

    feat: Navigare pe mai multe pagini

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
-
-import { useState } from 'react';
-import QuickNote from './QuickNote';
-import TodoList from './TodoList';
-import ContactForm from './ContactForm';
-import Clock from './Clock';
-import ProjectList from './ProjectList';
-
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
-      <Clock />
-      <ProjectList />
-      <QuickNote />
-      <TodoList />
-
-      <p>Ai apasat de {count} ori</p>
-      <button style={{margin:"5px"}} onClick={() => setCount(count + 1)}>Click</button>
-      <button style={{margin:"5px"}} onClick={() => setCount(count - 1)}>Unclick</button>
-      <button style={{margin:"5px"}} onClick={() => setCount(0)}>Reset</button>
-      
-      <ContactForm />
-      
-    </div>
-);
-
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </BrowserRouter>
+  );
 }
-  
+
 export default App;
