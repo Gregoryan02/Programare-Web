55aa11743155c7850a68509d110ad63f501e6861
Haita Grigore <haitagrigore@gmail.com>
2026-04-22 13:04:09 +0300

    feat:adaugat pagina About

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
+import About from './pages/About';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
             <Route path="/projects" element={<Projects />} />
             <Route path="/contact" element={<Contact />} />
             <Route path = "*" element={<NotFound/>}/>
+            <Route path = "/about" element={<About/>}/>
           </Routes>
         </BrowserRouter>
   );
---
+++ src/pages/About.jsx
@@ -0,0 +1,21 @@
+function About(){
+    return(
+        <div>
+            <h2>About</h2>
+            <p>Nume: Haita Grigore</p>
+            <p>Educatie</p>
+                <ul>
+                    <li>Colegiul National Grigore Moisil Onesti <br/>Profil:Stiinte ale Naturii <br/>An:2020-2024</li>
+                    <li>Universitatea Transilvania din Brasov <br/>Facultatea:IESC <br/>Profil:Calculatoare <br/> An:2024-prezent </li>
+                </ul>
+            <p>Hobby-uri</p>
+            <ul>
+                <li>Starcraft 2</li>
+                <li>Fotografie</li>
+                <li>PCB Design</li>
+            </ul>
+        </div>
+    );
+}
+
+export default About;
