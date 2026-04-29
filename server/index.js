78a2a0686249a9d9682cfb56817ff842ef5f7357
Haita Grigore <haitagrigore@gmail.com>
2026-04-29 13:10:21 +0300

    feat:get pe proiecte salvate in memorie

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-
+const projects = [
+    { id: 1, title: "Pagina Personala", tech: "HTML, CSS", done: true },
+    { id: 2, title: "Calculator Buget", tech: "JS", done: true },
+    { id: 3, title: "Dashboard React", tech: "React", done: false },
+    { id: 4, title: "API Meteo", tech: "React, API", done: false },
+];
 // Prima ruta: raspunde la GET /
 app.get('/', function(req, res) {
     res.json({ message: 'Serverul functioneaza!' });
+});
+app.get('/api/projects', function(req,res){
+    res.json(projects);
 });
 // Porneste serverul
 app.listen(PORT, function() {
